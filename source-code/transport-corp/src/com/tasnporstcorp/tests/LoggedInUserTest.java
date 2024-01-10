0c8e04c19d52cb3690a00bff5a84862fa43b6116
CenturionTheMan <dziedziakmichal0@gmail.com>
2024-01-10 22:20:23 +0100

    ParameterizedTest/MethodSource-> done

--- source-code/transport-corp/src/com/tasnporstcorp/tests/LoggedInUserTest.java
+++ source-code/transport-corp/src/com/tasnporstcorp/tests/LoggedInUserTest.java
@@ -1,33 +1,51 @@
 package com.tasnporstcorp.tests;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import com.tasnporstcorp.app.DataBaseAPI;
 import com.tasnporstcorp.tests.Dane;
-
-// package com.tasnporstcorp.app.LoggedInUser
 
 public class LoggedInUserTest {
 
-    Dane dane;
+    static Dane dane;
 
     @BeforeAll
-    public void init()
+    public static void init()
     {
         dane = new Dane();
     }
 
+    public static Stream<Arguments> testAddFilterValue()
+    {
+        ArrayList<Arguments> res = new ArrayList<Arguments>();
+
+        for (int i = 0; i < dane.filterAttributes.length; i++) {
+            for (int j = 0; j < dane.filterConditions.length; j++) {
+                res.add(Arguments.of(dane.filterAttributes[i], dane.filterConditions[j]));
+            }
+        }
+
+        return res.stream();
+    }
+
     @ParameterizedTest()
-    @ValueSource(dane.filterAttributes[0], dane.filterConditions[0])
+    @MethodSource("testAddFilterValue")
     public void testAddFilter(String attribute, String condition)
     {
         dane.loggedInUser[0].addFilter(attribute, condition);        
     }
 
-    @ParameterizedTest(name = "Test {index} => {arguments}")
-    public void testRemoveFilter()
-    {
-
-    }    
+    // @ParameterizedTest(name = "Test {index} => {arguments}")
+    // public void testRemoveFilter()
+    // {
+        
+    // }    
 } 
